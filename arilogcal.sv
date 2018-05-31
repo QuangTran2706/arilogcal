1a02b39547181c1d098456bcbd0ecf1832c36c2b
khoi <6994441+khoi@users.noreply.github.com>
2018-05-31 22:00:41 +0700

    Finite state machine implementation

--- arilogcal.sv
+++ arilogcal.sv
@@ -1,3 +1,126 @@
+module arilogcalfsm(
+	input logic [3:0]optA, 
+	input logic [3:0]optB, 
+	input logic [2:0]doOpt, 
+	input logic equalTo, ac, clk,
+	output logic [7:0]seg0, seg1, seg2, seg3, seg4, seg5, seg6, seg7
+);
+
+	logic [2:0] last_do_opt;
+	logic [3:0] last_a;
+	logic [3:0] last_b;
+
+	logic [2:0] opt;
+	logic [3:0] a;
+	logic [3:0] b;
+
+	logic [4:0] res2;
+	logic [4:0] res1;
+	logic [4:0] res0;
+
+	typedef enum logic[1:0] { Reset, Acuumulate, Calculation } State;
+
+	State current_state;
+
+	always_ff@(posedge clk, negedge ac) begin
+			if (!ac) begin
+				res2 <= 17; // Turning the LEDS off
+				res1 <= 17;
+				res0 <= 17; 
+				opt <= 0;
+				a <= 0;
+				b <= 0;
+				current_state <= Reset;
+			end
+			else begin
+				case(current_state)
+					Reset: begin
+						res2 <= 17; // Turning the LEDS off
+						res1 <= 17;
+						res0 <= 17; 
+						opt <= 0;
+						a <= 0;
+						b <= 0;
+						if (last_a != optA || last_b != optB || last_do_opt != doOpt) begin
+							current_state <= Acuumulate;
+						end
+					   else if (!equalTo) current_state <= Calculation;
+						else if (!ac) current_state <= Reset;
+					end
+					Acuumulate: begin
+						a <= optA;
+						last_a <= optA;
+						b <= optB;
+						last_b <= optB;
+						opt <= doOpt; 
+						last_do_opt <= doOpt;
+						if (!equalTo) current_state <= Calculation;
+						else if (!ac) current_state <= Reset;
+					end
+					Calculation: begin
+						case(opt)
+							1: begin
+								res2 <= ((a + b) / 100) % 10;
+								res1 <= ((a + b) / 10) % 10;
+								res0 <= (a + b) % 10;
+							end
+							2: begin 
+								res2 <= ((a * b) / 100) % 10;
+								res1 <= ((a * b) / 10) % 10;
+								res0 <= (a * b) % 10;
+							end
+							3: begin 
+								if (b == 0) begin
+									res2 <= 14; // E
+									res1 <= 16; // r
+									res0 <= 16; // r
+								end
+								else begin
+									res2 <= ((a / b) / 100) % 10;
+									res1 <= ((a / b) / 10) % 10;
+									res0 <= (a / b) % 10;
+								end
+							end
+							4: begin 
+								res2 <= (a && b) ? 1 : 0;
+								res1 <= (a && b) ? 1 : 0;
+								res0 <= (a && b) ? 1 : 0;
+							end
+							5: begin 
+								res2 <= (a || b) ? 1 : 0;
+								res1 <= (a || b) ? 1 : 0;
+								res0 <= (a || b) ? 1 : 0;
+							end
+							default: begin
+								res2 <= 14; // E
+								res1 <= 16; // r
+								res0 <= 16; // r
+							end
+						endcase
+						if (last_a != optA || last_a != optB || last_do_opt != doOpt) begin
+							current_state <= Acuumulate;
+						end
+						else if (!ac) current_state <= Reset;
+					end
+					default: current_state <= Reset;
+				endcase
+			end
+	end 
+
+	DisplaySeg s7(.number((a / 10) % 10), .seg_display(seg7));
+	DisplaySeg s6(.number(a % 10), .seg_display(seg6));
+
+	DisplaySeg s5(.number((b / 10) % 10), .seg_display(seg5));
+	DisplaySeg s4(.number(b % 10), .seg_display(seg4));
+	
+	DisplaySeg o(.number(opt), .seg_display(seg3));
+	
+	DisplaySeg s2(.number(res2), .seg_display(seg2));
+	DisplaySeg s1(.number(res1), .seg_display(seg1));
+	DisplaySeg s0(.number(res0), .seg_display(seg0));
+endmodule
+
+
 module DisplaySeg(
 	input logic [4:0] number, 
 	output logic [7:0] seg_display);
@@ -24,110 +147,3 @@ module DisplaySeg(
 		endcase
 	end
 endmodule
-
-module arilogcal(
-	input logic [3:0]optA, 
-	input logic [3:0]optB, 
-	input logic [2:0]doOpt, 
-	input logic equalTo, ac, clk,
-	output logic [7:0]seg0, seg1, seg2, seg3, seg4, seg5, seg6, seg7
-);
-	logic last_equal_to;
-	logic [2:0] last_do_opt;
-	logic [3:0] last_a;
-	logic [3:0] last_b;
-	
-	logic [2:0] opt;
-	logic [3:0] a;
-	logic [3:0] b;
-	
-	logic [4:0] res2;
-	logic [4:0] res1;
-	logic [4:0] res0;
-
-	always_ff@(posedge clk, negedge ac) begin
-		if (!ac) begin
-			res2 <= 17; // Turning the LEDS off
-			res1 <= 17;
-			res0 <= 17; 
-			opt <= 0;
-			a <= 0;
-			b <= 0;
-		end
-		else if (!equalTo && last_equal_to == 1) begin
-			case(opt)
-			1: begin // Additional
-				res2 <= ((a + b) / 100) % 10;
-				res1 <= ((a + b) / 10) % 10;
-				res0 <= (a + b) % 10;
-			end
-			2: begin // Multiplication
-				res2 <= ((a * b) / 100) % 10;
-				res1 <= ((a * b) / 10) % 10;
-				res0 <= (a * b) % 10;
-			end
-			3: begin // Division
-				if (b == 0) begin
-					res2 <= 14; // E
-					res1 <= 16; // r
-					res0 <= 16; // r
-				end
-				else begin
-					res2 <= ((a / b) / 100) % 10;
-					res1 <= ((a / b) / 10) % 10;
-					res0 <= (a / b) % 10;
-				end
-			end
-			4: begin // Logical And
-				res2 <= (a && b) ? 1 : 0;
-				res1 <= (a && b) ? 1 : 0;
-				res0 <= (a && b) ? 1 : 0;
-			end
-			5: begin // Logical OR
-				res2 <= (a || b) ? 1 : 0;
-				res1 <= (a || b) ? 1 : 0;
-				res0 <= (a || b) ? 1 : 0;
-			end
-			default: begin
-				res2 <= 14; // E
-				res1 <= 16; // r
-				res0 <= 16; // r
-			end
-			endcase
-			last_equal_to <= equalTo;
-		end
-		else if (last_a != optA) begin
-			a <= optA;
-			last_a <= optA;
-		end	
-		else if (last_b != optB) begin
-			b <= optB;
-			last_b <= optB;
-		end	
-		else if (doOpt != last_do_opt) begin
-			opt <= doOpt; 
-			last_do_opt <= doOpt;
-		end
-		else begin
-			last_equal_to <= equalTo;
-			last_do_opt <= doOpt;
-			last_a <= optA;
-			last_b <= optB;
-		end
-	end
-	DisplaySeg s7(.number((a / 10) % 10), .seg_display(seg7));
-	DisplaySeg s6(.number(a % 10), .seg_display(seg6));
-
-	DisplaySeg s5(.number((b / 10) % 10), .seg_display(seg5));
-	DisplaySeg s4(.number(b % 10), .seg_display(seg4));
-	
-	DisplaySeg o(.number(opt), .seg_display(seg3));
-	
-	DisplaySeg s2(.number(res2), .seg_display(seg2));
-	DisplaySeg s1(.number(res1), .seg_display(seg1));
-	DisplaySeg s0(.number(res0), .seg_display(seg0));
-endmodule
-
-
-
-	
